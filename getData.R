8107122c7836de7ea85de79423e2650f3053f0de
mrchypark <mrchypark@gmail.com>
2017-04-16 13:37:09 +0900

    code done

--- getData.R
+++ getData.R
@@ -14,9 +14,9 @@ maker <- do.call(rbind.data.frame, maker$Makes)[-6,] %>%unique()
 
 lmdata<-c()
 for (i in 1:nrow(maker)){
-  print(i)
+#  print(i)
   te <- GET(paste0("https://sg-api.gobear.com/api/cars/makes/",maker$ID[i],"/models"))
-  print(te)
+#  print(te)
   models <- content(te,"parsed")
   models <- do.call(rbind.data.frame, models$Models)
   models$make <- maker$Name[i]
@@ -28,10 +28,11 @@ save(lmdata,file="modelNum.RData")
 
 load("modelNum.RData")
 
-birth <- "1987-01-01"
+birth <- "1976-01-21"
 e <- "30"
 registerYear <- "2013"
-carInfo <- paste0("&make=",lmdata$makeID,"&model=",lmdata$ID)
+carInfo <- lmdata
+#carInfo <- paste0("&make=",lmdata$makeID,"&model=",lmdata$ID)
 plans <- "Comprehensive"
 maritalStatus <- c("Married","Single")
 gender <- c("Male","Female")
@@ -50,13 +51,16 @@ remDr <- remoteDriver(remoteServerAddr="192.168.99.100", port = 4445L,
                       browserName="firefox")
 remDr$open()
 
-for(i in carInfo){
+top4 <- c()
+
+for(i in nrow(carInfo)){
   for(j in maritalStatus){
     for(k in gender){
       for(l in drivingExperience){
         for(m in noClaimValue){
+          carInfoUrl <- paste0("&make=",lmdata$makeID[i],"&model=",lmdata$ID[i])
           url <- paste0(home,"birth=",birth,"&e=",e,"&registerYear=",registerYear,
-                         i,"&plans=",plans,"&maritalStatus=",j,
+                        carInfoUrl,"&plans=",plans,"&maritalStatus=",j,
                          "&gender=",k,"&drivingExperience=",l,
                          "&noClaimValue=",m,"&occupation=",occupation,
                          "&offPeak=",offPeak)
@@ -66,11 +70,27 @@ for(i in carInfo){
           tem <- remDr$getPageSource()[[1]]
           tem <- read_html(tem)
           comp <- tem %>% html_nodes("h1") %>% html_text()
-          comp
           plan <- tem %>% html_nodes("h2") %>% html_text()
-          plan
           price <- tem %>% html_nodes("span.value") %>% html_text()
-          price
+          if(length(price)!=length(comp)){
+            tem <- sample("NA",10,replace = T)
+            price <- c(price,tem)
+            price <- price[1:10]
+          }
+          
+          tem <- data.frame(Gender=k,MaritalStatus=j,Occupation=occupation,
+                            DOB=birth,AgeOfDriver=e,LicenceYears=registerYear,
+                            NCD=m,OffPeak=offPeak,
+                            DrivingExperience=l,
+                            Make=lmdata$make[i],Model=lmdata$Name[i],CC=lmdata$Capacity[i],
+                            company1=comp[1],Plan=plan[1],Price=price[1],
+                            company1=comp[2],Plan=plan[2],Price=price[2],
+                            company1=comp[3],Plan=plan[3],Price=price[3],
+                            company1=comp[4],Plan=plan[4],Price=price[4]
+                            )
+          
+          top4 <- rbind(top4,tem)
+          print(tem)
         }
       }
     }
