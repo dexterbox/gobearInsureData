47032aa2bfc0882b4933ddd2ad3d53c931257c3d
mrchypark <mrchypark@gmail.com>
2017-04-17 02:46:47 +0900

    done without knit

--- getData.R
+++ getData.R
@@ -1,6 +1,5 @@
 library(selectr)
 library(rvest)
-library(RSelenium)
 library(httr)
 
 options(stringsAsFactors = F)
@@ -14,9 +13,8 @@ maker <- do.call(rbind.data.frame, maker$Makes)[-6,] %>%unique()
 
 lmdata<-c()
 for (i in 1:nrow(maker)){
-#  print(i)
+  print(i)
   te <- GET(paste0("https://sg-api.gobear.com/api/cars/makes/",maker$ID[i],"/models"))
-#  print(te)
   models <- content(te,"parsed")
   models <- do.call(rbind.data.frame, models$Models)
   models$make <- maker$Name[i]
@@ -29,11 +27,8 @@ save(lmdata,file="modelNum.RData")
 load("modelNum.RData")
 
 birth <- "1976-01-21"
-e <- "30"
 registerYear <- "2013"
 carInfo <- lmdata
-#carInfo <- paste0("&make=",lmdata$makeID,"&model=",lmdata$ID)
-plans <- "Comprehensive"
 maritalStatus <- c("Married","Single")
 gender <- c("Male","Female")
 drivingExperience <- c("None","One","Two","Three","Four","Five","Six","Seven","Eight","Nine",
@@ -42,14 +37,7 @@ noClaimValue <- c(0,10,20,30,40,50)
 occupation <- "Indoor"
 offPeak <- "false"
 
-home<-"https://www.gobear.com/sg/insurance/car/quote-online?"
-
-## docker run -d -p 4445:4444 --name ff selenium/standalone-firefox:3.3.0
-## docker inspect ff .. IPAddress > if using in docker
-
-remDr <- remoteDriver(remoteServerAddr="192.168.99.100", port = 4445L, 
-                      browserName="firefox")
-remDr$open()
+home<-"https://sg-api.gobear.com/api/insurance/car?CoverageFiltrations=&"
 
 top4 <- c()
 
@@ -58,48 +46,49 @@ for(i in nrow(carInfo)){
     for(k in gender){
       for(l in drivingExperience){
         for(m in noClaimValue){
-          carInfoUrl <- paste0("&make=",lmdata$makeID[i],"&model=",lmdata$ID[i])
-          url <- paste0(home,"birth=",birth,"&e=",e,"&registerYear=",registerYear,
-                        carInfoUrl,"&plans=",plans,"&maritalStatus=",j,
-                         "&gender=",k,"&drivingExperience=",l,
-                         "&noClaimValue=",m,"&occupation=",occupation,
-                         "&offPeak=",offPeak)
+          # carInfoUrl <- paste0("&make=",lmdata$makeID[i],"&model=",lmdata$ID[i])
+          url <- paste0(home,"DateOfBirth=",birth,"&RegisterYear=",registerYear,
+                        "&ModelGuid=",lmdata$ID[i],"&MaritalStatus=",j,
+                         "&Gender=",k,"&DrivingExperience=",l,
+                         "&NoClaimValue=",m,"&Occupation=",occupation,
+                         "&OffPeak=",offPeak)
 
-          remDr$navigate(url)
-          Sys.sleep(1)
-          tem <- remDr$getPageSource()[[1]]
-          tem <- read_html(tem)
-          comp <- tem %>% html_nodes("h1") %>% html_text()
-          plan <- tem %>% html_nodes("h2") %>% html_text()
-          price <- tem %>% html_nodes("span.value") %>% html_text()
-          if(length(price)!=length(comp)){
-            tem <- sample("NA",10,replace = T)
-            price <- c(price,tem)
-            price <- price[1:10]
-          }
+          tem <- GET(url)
+          tem <- content(tem,"parsed")
+          
+          candidate <- sapply(tem$Insurances, function(x) x$TotalScore)
+          Company <- sapply(tem$Insurances, function(x) x$InsurerName)
+          Plan <- sapply(tem$Insurances, function(x) x$PlanName)
+          Price <- sapply(tem$Insurances, function(x) x$CheckoutOptions[[1]]$Premium)
+          
+          Company <- Company[candidate==25]
+          Plan <- Plan[candidate==25]
+          Price <- Price[candidate==25]
+          Price <- ifelse(Price==0,1000000,Price)
+          Price <- Price[order(Price)][1:4]
+          Plan <- Plan[order(Price)][1:4]
+          Company <- Company[order(Price)][1:4]
+          Price <- ifelse(Price==1000000,"NoPrice",Price)
           
           tem <- data.frame(Gender=k,MaritalStatus=j,Occupation=occupation,
                             DOB=birth,AgeOfDriver=e,LicenceYears=registerYear,
                             NCD=m,OffPeak=offPeak,
                             DrivingExperience=l,
                             Make=lmdata$make[i],Model=lmdata$Name[i],CC=lmdata$Capacity[i],
-                            company1=comp[1],Plan=plan[1],Price=price[1],
-                            company1=comp[2],Plan=plan[2],Price=price[2],
-                            company1=comp[3],Plan=plan[3],Price=price[3],
-                            company1=comp[4],Plan=plan[4],Price=price[4]
+                            company1=Company[1],Plan1=Plan[1],Price1=Price[1],
+                            company2=Company[2],Plan2=Plan[2],Price2=Price[2],
+                            company3=Company[3],Plan3=Plan[3],Price3=Price[3],
+                            company4=Company[4],Plan4=Plan[4],Price4=Price[4]
                             )
           
           top4 <- rbind(top4,tem)
           print(tem)
+          write.csv(top4,"./data.csv",row.names = F)
         }
       }
     }
   }
 }
-
-
-
-
 
 
 
