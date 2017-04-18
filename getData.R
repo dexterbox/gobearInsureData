c679bd5f2662bbc947579621e7fa9d030baf0e46
mrchypark <mrchypark@gmail.com>
2017-04-18 04:17:22 +0000

    clean

--- getData.R
+++ getData.R
@@ -94,55 +94,55 @@ for(i in nrow(lmdata)){
 
 
 
-
-for(i in nrow(lmdata)){
-  for(j in maritalStatus){
-    for(k in gender){
-      for(l in drivingExperience){
-        for(m in noClaimValue){
-
-getData <- function(lmdata,maritalStatus,gender,drivingExperience,noClaimValue)
-urlData1 <- merge(data.frame(drivingExperience),data.frame(noClaimValue))
-urlData2 <- merge(data.frame(gender),data.frame(maritalStatus))
-urlData <- merge(urlData1,urlData2)
-
-url <- paste0(home,"DateOfBirth=",birth,"&RegisterYear=",registerYear,
-              "&ModelGuid=",lmdata$ID[i],"&MaritalStatus=",urlData$maritalStatus,
-              "&Gender=",urlData$gender,"&DrivingExperience=",urlData$drivingExperience,
-              "&NoClaimValue=",urlData$noClaimValue,"&Occupation=",occupation,
-              "&OffPeak=",offPeak)
-
-registerDoParallel(32)
-
-te<-foreach(uu = url, 
-            .combine = list,
-            .multicombine = TRUE)  %dopar%  
-  content(GET(uu),"parsed")
-
-te<-lapply(url,function(x) content(GET(x),"parsed"))
-tem <- content(tem,"parsed")
-
-candidate <- sapply(tem$Insurances, function(x) x$TotalScore)
-Company <- sapply(tem$Insurances, function(x) x$InsurerName)
-Plan <- sapply(tem$Insurances, function(x) x$PlanName)
-Price <- sapply(tem$Insurances, function(x) x$CheckoutOptions[[1]]$Premium)
-
-Company <- Company[candidate==25]
-Plan <- Plan[candidate==25]
-Price <- Price[candidate==25]
-Price <- ifelse(Price==0,1000000,Price)
-Price <- Price[order(Price)][1:4]
-Plan <- Plan[order(Price)][1:4]
-Company <- Company[order(Price)][1:4]
-Price <- ifelse(Price==1000000,"NoPrice",Price)
-
-tem <- data.frame(Gender=k,MaritalStatus=j,Occupation=occupation,
-                  DOB=birth,AgeOfDriver=e,LicenceYears=registerYear,
-                  NCD=m,OffPeak=offPeak,
-                  DrivingExperience=l,
-                  Make=lmdata$make[i],Model=lmdata$Name[i],CC=lmdata$Capacity[i],
-                  company1=Company[1],Plan1=Plan[1],Price1=Price[1],
-                  company2=Company[2],Plan2=Plan[2],Price2=Price[2],
-                  company3=Company[3],Plan3=Plan[3],Price3=Price[3],
-                  company4=Company[4],Plan4=Plan[4],Price4=Price[4]
-)
+# 
+# for(i in nrow(lmdata)){
+#   for(j in maritalStatus){
+#     for(k in gender){
+#       for(l in drivingExperience){
+#         for(m in noClaimValue){
+# 
+# getData <- function(lmdata,maritalStatus,gender,drivingExperience,noClaimValue)
+# urlData1 <- merge(data.frame(drivingExperience),data.frame(noClaimValue))
+# urlData2 <- merge(data.frame(gender),data.frame(maritalStatus))
+# urlData <- merge(urlData1,urlData2)
+# 
+# url <- paste0(home,"DateOfBirth=",birth,"&RegisterYear=",registerYear,
+#               "&ModelGuid=",lmdata$ID[i],"&MaritalStatus=",urlData$maritalStatus,
+#               "&Gender=",urlData$gender,"&DrivingExperience=",urlData$drivingExperience,
+#               "&NoClaimValue=",urlData$noClaimValue,"&Occupation=",occupation,
+#               "&OffPeak=",offPeak)
+# 
+# registerDoParallel(32)
+# 
+# te<-foreach(uu = url, 
+#             .combine = list,
+#             .multicombine = TRUE)  %dopar%  
+#   content(GET(uu),"parsed")
+# 
+# te<-lapply(url,function(x) content(GET(x),"parsed"))
+# tem <- content(tem,"parsed")
+# 
+# candidate <- sapply(tem$Insurances, function(x) x$TotalScore)
+# Company <- sapply(tem$Insurances, function(x) x$InsurerName)
+# Plan <- sapply(tem$Insurances, function(x) x$PlanName)
+# Price <- sapply(tem$Insurances, function(x) x$CheckoutOptions[[1]]$Premium)
+# 
+# Company <- Company[candidate==25]
+# Plan <- Plan[candidate==25]
+# Price <- Price[candidate==25]
+# Price <- ifelse(Price==0,1000000,Price)
+# Price <- Price[order(Price)][1:4]
+# Plan <- Plan[order(Price)][1:4]
+# Company <- Company[order(Price)][1:4]
+# Price <- ifelse(Price==1000000,"NoPrice",Price)
+# 
+# tem <- data.frame(Gender=k,MaritalStatus=j,Occupation=occupation,
+#                   DOB=birth,AgeOfDriver=e,LicenceYears=registerYear,
+#                   NCD=m,OffPeak=offPeak,
+#                   DrivingExperience=l,
+#                   Make=lmdata$make[i],Model=lmdata$Name[i],CC=lmdata$Capacity[i],
+#                   company1=Company[1],Plan1=Plan[1],Price1=Price[1],
+#                   company2=Company[2],Plan2=Plan[2],Price2=Price[2],
+#                   company3=Company[3],Plan3=Plan[3],Price3=Price[3],
+#                   company4=Company[4],Plan4=Plan[4],Price4=Price[4]
+# )
